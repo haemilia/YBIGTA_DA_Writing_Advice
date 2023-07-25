eb6250ff7cd5b37cb3a85363be95860b1cd2445f
Oh-dongha <122213954+Oh-dongha@users.noreply.github.com>
2023-07-25 19:30:59 +0900

    Add files via upload

--- web/server.py
+++ web/server.py
@@ -7,8 +7,8 @@ app.config['DEBUG'] = True
 CORS(app)
 
 
-@app.route('/react_to_flask', methods=['POST'])
-def react_to_flask():
+@app.route('/react_to_flask_1', methods=['POST'])
+def react_to_flask_1():
     data = request.get_json()
     category = data['category']
     text = data['text']
@@ -18,6 +18,15 @@ def react_to_flask():
     #-------------------------------------------------------
     return jsonify({'modifiedText': modifiedText})
 
+@app.route('/react_to_flask_2', methods=['POST'])
+def react_to_flask_2():
+    data = request.get_json()
+    Mtext = data['SecondModifiedText']
+    #-------------------------------------------------------
+    #여기에 맞춤법 맞추는 코드가 들어가야함 근데 적용이 안됨
+    analysisResult = f'abc{Mtext}.'
+    #-------------------------------------------------------
+    return jsonify({'analysisResult': analysisResult})
 
 if __name__ == '__main__':
     app.run(port="5000",  debug=True)
